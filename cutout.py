bdb20604226814c02b68c017d5e678a6e10c9937
Robert Morgan <robert.morgan@wisc.edu>
2021-04-15 14:30:05 -0500

    update for single epoch images

--- cutout.py
+++ cutout.py
@@ -10,6 +10,7 @@
 
 import glob
 import os
+from pathlib import Path
 import sys
 
 from astropy.coordinates import SkyCoord
@@ -37,70 +38,103 @@ class CutoutProducer:
 
     """
 
-    def __init__(self, tilename, cutout_size, bands="griz",
-                 metadata_path='/data/des81.b/data/stronglens/Y6_CUTOUT_METADATA/',
-                 coadds_path='/data/des40.b/data/des/y6a2/coadd/image/'):
+    def __init__(self, field, ccd, season, cutout_size, outdir, test=False,
+                 image_path = '/afs/hep.wisc.edu/home/ramorgan2/DeepTransientSims/Data/',
+                 metadata_path = '/afs/hep.wisc.edu/home/ramorgan2/DeepTransientSims/Data/',
+                 catalog_path = '/afs/hep.wisc.edu/home/ramorgan2/DeepTransientSims/Data/'):
 
         """
         Initialize a CutoutProducer.
 
-        :param tilename: (str) name of DES tile; something like 'DES0536-5457'
-        :param cutout_size: (int) side length in pixels of desired cutouts
-        :param bands: (str) bands to include; something like "griz" or "grizY"
-        :param coadds_path: (str) path relative to the COADD images (organized as tiles)
         """
+        self.image_path = image_path
         self.metadata_path = metadata_path
-        self.coadds_path = coadds_path 
+        self.catalog_path = catalog_path 
         self.metadata_suffix = ""
-        self.tilename = tilename
+        self.test = test
+        self.field = field
+        self.ccd = ccd
+        self.season = season
         self.cutout_size = cutout_size
-        self.bands = bands
-        return
+        self.outdir = outdir
+
 
     def read_metadata(self):
         """
-        Read the metadata for the tile into a Pandas DataFrame
+        Read the metadata for the observations into a Pandas DataFrame
         """
-        filename = self.metadata_path + self.tilename + self.metadata_suffix
+        filename = self.metadata_path + self.field.lower() + "_metadata_seasons.tab" + self.metadata_suffix
         if not os.path.exists(filename):
-            raise IOError(f"{self.tilename} is not a valid tilename")
+            raise IOError(f"{filename} not found")
+            
+        # Trim to relevant metadata
+        df = pd.read_csv(filename, delim_whitespace=True)
+        df['CCD'] = df['FILENAME'].str.extract("_c(.*?)_").values.astype(int)
+        df = df[(df['SEASON'].values == self.season.upper()) &
+                (df['CCD'].values == self.ccd)
+                ].copy().reset_index(drop=True)
 
-        self.metadata = pd.read_csv(filename, delim_whitespace=True)
-        return
+        if len(df) == 0:
+            raise ValueError(f"SEASON {self.season} and CCD {self.ccd} produced empty metadata")
 
-    def get_tile_filename(self, band):
+        # Drop nites that don't have all of griz, select best psf if two images exist
+        self.metadata = {}
+        nite_groups = df.groupby('NITE')
+        for (nite, md) in nite_groups:
+            flt_groups = md.groupby('FILTER')
+            mds_ = {}
+            for (flt, md_) in flt_groups:
+                mds_[flt] = md_.iloc[np.argmin(md_['FWHM'].values)]
+            if len(mds_) == 4:
+                self.metadata[nite] = mds_
+
+
+        del df
+        if len(self.metadata) == 0:
+            raise ValueError(f"Trimming by nite and filter produced empty metadata")
+
+        # sort by increasing MJD
+        self.nites = sorted(list(self.metadata.keys()))
+
+        # trim if testing
+        if self.test:
+            self.metadata = {self.nites[0]: self.metadata[self.nites[0]], 
+                             self.nites[1]: self.metadata[self.nites[1]]}
+            self.nites = self.nites[0:2]
+
+    def read_catalog(self):
         """
-        construct the filepath to the coadd tile
-
-        :param band: (str) one of ('g', 'r', 'i', 'z', 'Y')
-        :return: path: (str) absolute path to tile
+        Read the catalog and trim to necessary rows
         """
+        filename = self.catalog_path + "deep_catalog.csv"
+        if not os.path.exists(filename):
+            raise IOError(f"{filename} not found")
 
-        guess = os.path.join(self.coadds_path, self.tilename,
-                             f'{self.tilename}_r*_{band}.fits.fz')
+        # Trim to relevant catalog
+        df = pd.read_csv(filename)
+        self.catalog = df[(df['CCD'].values == self.ccd) &
+                          (df['FIELD'].values == 'SN-' + self.field.upper())
+                          ].copy().reset_index(drop=True)
+        del df
+        if len(self.catalog) == 0:
+            raise ValueError(f"CCD {self.ccd} and FIELD {self.field} produced an empty catalog")
 
-        matches = glob.glob(guess)
+        # trim if testing
+        if self.test:
+            self.catalog = self.catalog.sample(5)
 
-        if len(matches) > 1:
-            return ValueError('error - more than one possible coadd')
-        elif not matches:
-            raise ValueError('no images found')
 
-        return matches[0]
-
-    def read_tile_image(self, band):
+    def read_image(self, filename):
         """
         Open a fits file and return the image array and the WCS
 
-        :param band: (str) one of ('g', 'r', 'i', 'z', 'Y')
+        :param filename: (str) name of file to read
         :return: image: (np.Array) the image data contained in the file
         :return: wcs: (astropy.WCS) the wcs for the file
         """
-        tile_filename = self.get_tile_filename(band)
-
-        f = fits.open(tile_filename, mode='readonly')
-        image = f[1].data
-        wcs = WCS(f[1].header)
+        f = fits.open(filename, mode='readonly')
+        image = f["SCI"].data
+        wcs = WCS(f["SCI"].header)
         f.close()
         return image, wcs
 
@@ -111,19 +145,19 @@ class CutoutProducer:
         :return: ras: (np.Array) all RA values of objects in the tile
         :return: decs: (np.Array) all DEC values of objects in the tile
         """
-        if not hasattr(self, "metadata"):
-            self.read_metadata()
+        if not hasattr(self, "catalog"):
+            self.read_catalog()
 
-        return self.metadata['RA'].values, self.metadata['DEC'].values
+        return self.catalog['RA'].values, self.catalog['DEC'].values
 
     def get_coadd_ids(self):
         """
         Get an array of all coadd object ids
         """
-        if not hasattr(self, "metadata"):
-            self.read_metadata()
+        if not hasattr(self, "catalog"):
+            self.read_catalog()
 
-        self.coadd_ids = np.array(self.metadata['COADD_OBJECT_ID'].values, dtype=int)
+        self.coadd_ids = self.catalog['COADD_OBJECT_ID'].values.astype(int)
         return
 
     def get_object_xy(self, wcs):
@@ -186,7 +220,7 @@ class CutoutProducer:
                      x - width//2: x + width//2 + 1]
 
 
-    def combine_bands(self):
+    def combine_bands(self, g_filename, r_filename, i_filename, z_filename):
         """
         Get cutouts (both image and psf) from all bands and stack into one array
 
@@ -196,12 +230,11 @@ class CutoutProducer:
         if not hasattr(self, "coadd_ids"):
             self.get_coadd_ids()
 
-        image_array = np.empty((len(self.bands), len(self.coadd_ids), self.cutout_size, self.cutout_size), dtype=np.double)
-        psf_array = np.empty((len(self.bands), len(self.coadd_ids), self.psf_cutout_size, self.psf_cutout_size), dtype=np.double)
+        image_array = np.empty((4, len(self.coadd_ids), self.cutout_size, self.cutout_size), dtype=np.double)
         
-        for i, band in enumerate(self.bands):
+        for i, filename in enumerate([g_filename, r_filename, i_filename, z_filename]):
             # Open image file
-            image, wcs = self.read_tile_image(band)
+            image, wcs = self.read_image(filename)
 
             # Cutout images
             image_cutouts = self.cutout_objects(image, wcs)
@@ -233,62 +266,79 @@ class CutoutProducer:
 
         return int_arr, original_min, shifted_max
 
+    def format_filename(self, base_filename):
+        return f"{self.image_path}{self.season}/{base_filename}.fz"
 
-    def produce_cutout_file(self, image_array, out_dir=''):
+    def cutout_all_epochs(self):
         """
-        Organize cutout data into an output file
+        loop through epochs and stack cutouts, track and organize metadata
 
-        :param image_array: (np.array) contains all images. Has
-                            shape = (len(coadd_ids), num_bands, cutout_size, cutout_size)
-        :param out_dir: (str) path to out directory
-           - leave as default '' for current directory
-
+        output_shape = (objects, band, height, width)
         """
-        # Scale the image and psf arrays
-        image_array, img_min, img_scale = self.scale_array_to_ints(image_array)
+        output = {}
+        for nite in self.nites:
+            # Get filenames from metadata
+            g_filename = self.format_filename(self.metadata[nite]['g']['FILENAME'])
+            r_filename = self.format_filename(self.metadata[nite]['r']['FILENAME'])
+            i_filename = self.format_filename(self.metadata[nite]['i']['FILENAME'])
+            z_filename = self.format_filename(self.metadata[nite]['z']['FILENAME'])
 
-        # Make an empty PRIMARY HDU
-        primary = fits.PrimaryHDU()
+            # Cutout objects
+            image_array = self.combine_bands(g_filename, r_filename, i_filename, z_filename)
 
-        # Make the COADD_ID HDU
-        if not hasattr(self, "coadd_ids"):
-            self.get_coadd_ids()
-        col = fits.Column(name='COADD_OBJECT_ID', array=self.coadd_ids, format='J')
-        coadd_ids = fits.BinTableHDU.from_columns([col], name="CUTOUT_ID")
+            # Scale array
+            image_array, img_min, img_scale = self.scale_array_to_ints(image_array)
 
-        # Make the IMAGE HDU
-        image = fits.ImageHDU(image_array, name="IMAGE")
+            # Store images
+            output[nite] = {'IMG': image_array,
+                            'IMG_MIN': img_min,
+                            'IMG_SCALE': img_scale,
+                            'METADATA': self.metadata[nite]}
 
-        # Make the img_min and img_scale HDUs
-        img_min = fits.ImageHDU(img_min, name="IMG_MIN")
-        img_scale = fits.ImageHDU(img_scale, name="IMG_SCALE")
-
-        # Write the file
-        hdu_list = fits.HDUList([primary, coadd_ids, image, img_min, img_scale])
-        if not out_dir.endswith('/') and out_dir != '':
-            out_dir += '/'
-        hdu_list.writeto(f'{out_dir}{self.tilename}.fits', overwrite=True)
-        return
+        output['CATALOG'] = self.catalog
+        output['NITES'] = self.nites
+        return output
+                     
+    def get_outfile_name(self):
+        return f"{self.outdir}{self.field}_{self.season}_{self.ccd}.npy"
+        
+    def save_output(self, out_dict):
+        outfile_name = self.get_outfile_name()
+        np.save(outfile_name, out_dict, allow_pickle=True)
 
 
 if __name__ == "__main__":
-    assert len(sys.argv) == 2, "Tilename must be given as a a command-line argument"
-    tilename = sys.argv[1]
+    #assert len(sys.argv) == 2, "Tilename must be given as a a command-line argument"
+    #tilename = sys.argv[1]
     CUTOUT_SIZE = 45
-    BANDS = "griz"
-    OUTDIR = "/data/des81.b/data/stronglens/Y6_CUTOUT_IMAGES/"
+    field = 'X1'
+    ccd = 33
+    season = 'Y1'
+    #OUTDIR = "/data/des81.b/data/stronglens/Y6_CUTOUT_IMAGES/"
 
     # Make a CutoutProducer for the tile
-    cutout_prod = CutoutProducer(tilename, CUTOUT_SIZE, bands=BANDS)
+    cutout_prod = CutoutProducer(field, ccd, season, CUTOUT_SIZE, outdir="", test=True)
 
-    # Quit if missing files
-    for band in cutout_prod.bands:
-        tile_path = cutout_prod.get_tile_filename(band)
-        assert os.path.exists(tile_path), "Coadd image should exist"
-        
-    # Produce the cutouts
+    # Check that we can save the output at the end
+    outfile_name = cutout_prod.get_outfile_name()
+    try:
+        Path(outfile_name).touch(exist_ok=True)
+    except Exception:
+        raise OSError(f"{outfile_name} cannot be created")    
+
+    # Read metadata and catalog
     cutout_prod.read_metadata()
-    image_array = cutout_prod.combine_bands()
-    cutout_prod.produce_cutout_file(image_array, out_dir=OUTDIR)
+    cutout_prod.read_catalog()
+
+    # Check that all the files exist
+    for nite, flt_dict in cutout_prod.metadata.items():
+        for md in flt_dict.values():
+            if not os.path.exists(cutout_prod.format_filename(md['FILENAME'])):
+                raise OSError(f"{cutout_prod.format_filename(md['FILENAME'])} not found")
+
+    # Do the thing
+    cutout_prod.save_output(cutout_prod.cutout_all_epochs())
+
+
 
 
